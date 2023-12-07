84f6304d21e27c186ae8e2405e885f2965dec029
daniel <18262962+danieldonatsch@users.noreply.github.com>
2023-12-07 17:54:46 +0100

    Added a classifier loss to generate real numbers, numbers which are requested.

--- train_gan.py
+++ train_gan.py
@@ -2,7 +2,6 @@
 https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
 """
 import argparse
-import os
 import random
 import torch
 import torch.nn as nn
@@ -10,13 +9,12 @@ import torch.nn.parallel
 import torch.optim as optim
 import torch.utils.data
 import torchvision.datasets as dset
-from torchvision import datasets, transforms
+import torchvision.transforms as transf
 import torchvision.utils as vutils
-import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 from nets import Generator, Discriminator
+from train_classifier import ClassifierNet
 
 
 def get_user_input():
@@ -58,11 +56,11 @@ def get_dataloader(args) -> torch.utils.data.DataLoader:
     :param args: args from ArgumentParser
     :return: torch.utils.data.DataLoader object
     """
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
+    transform = transf.Compose([
+        transf.ToTensor(),
+        transf.Normalize((0.5,), (0.5,))
         ])
-    dataset = datasets.MNIST('../data', train=True, download=True, transform=transform)
+    dataset = dset.MNIST('../data', train=True, download=True, transform=transform)
     return torch.utils.data.DataLoader(dataset, batch_size=args.batch_size)
 
 
@@ -106,24 +104,54 @@ def get_nets(device, args) -> tuple:
     """
     # Create the generator
     ngpu = 1    # TODO make it an argument
-    g = Generator(ngpu, 1).to(device)
+    g = Generator(ngpu, 2).to(device)
     # Create the Discriminator
     d = Discriminator(ngpu).to(device)
+    # Create the Classifier, needed for the loss
+    c = ClassifierNet().to(device)
 
     # Handle multi-GPU if desired
     if (device.type == 'cuda') and (ngpu > 1):
         g = nn.DataParallel(g, list(range(ngpu)))
         d = nn.DataParallel(d, list(range(ngpu)))
+        c = nn.DataParallel(c, list(range(ngpu)))
 
     # Apply the weights_init function to randomly initialize all weights to mean=0, stdev=0.02.
     g.apply(weights_init)
     d.apply(weights_init)
+    # Load existing classifier weights
+    c.load_state_dict(torch.load('mnist_classifier_acc_9917-10000.pt'))
+    c.eval()
 
     # Print the model
     #print(g)
     #print(d)
+    #print(c)
 
-    return g, d
+    return g, d, c
+
+
+def create_generator_input(batch_size, device):
+    """Creates input for the generator.
+
+    For each input a 2x7x7 tensor,
+        one 7x7 "layer" with the number which should be darwn in the image,
+        one 7x7-"layer" with random values.
+    Total output has batch_size x 2 x 7 x 7, plus a list with the target numbers
+
+    :param batch_size: int, batch size
+    :param device: torch.device
+    :return: generator_input, expected numbers
+    """
+    # Generate batch of generator input, consisting of noise and the target number
+    noise = torch.randn(batch_size, 1, 7, 7, device=device)
+    target_nums = torch.randint(0, 10, size=(batch_size, 1, 1, 1), device=device)
+    req_num = (target_nums / 4.5) - 1.0
+    req_num = torch.repeat_interleave(req_num, 7, 2)
+    req_num = torch.repeat_interleave(req_num, 7, 3)
+    generator_input = torch.cat((req_num, noise), dim=1)
+
+    return generator_input, target_nums.view(-1)
 
 
 def train_discriminator():
@@ -145,13 +173,13 @@ def main():
 
     dataloader = get_dataloader(args)
 
-    generator, discriminator = get_nets(device, args)
+    generator, discriminator, classifier = get_nets(device, args)
 
     # Initialize the ``BCELoss`` function
     criterion = nn.BCELoss()
 
     # Create batch of latent vectors that we will use to visualize the progression of the generator
-    fixed_noise = torch.randn(64, 1, 7, 7, device=device)
+    fixed_gen_in, fixed_exp_nums = create_generator_input(64, device)
 
     # Establish convention for real and gen_imgs labels during training
     real_label = 1.
@@ -170,13 +198,16 @@ def main():
     print("Starting Training Loop...")
     # For each epoch
     for epoch in range(args.epochs):
+        alpha = 0.5 + 0.5 * epoch/args.epochs  # The weight of the number classifier
+        print("alpha:", alpha)
+        loss_disc, loss_gen, D_x, D_G_z1, D_G_z2, gen_acc = 0, 0, 0, 0, 0, 0
         # For each batch in the dataloader
         for i, data in enumerate(dataloader, 0):
 
             ############################
             # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
             ###########################
-            ## Train with all-real batch
+            # # Train with all-real batch
             discriminator.zero_grad()
             # Format batch
             real_imgs = data[0].to(device)
@@ -185,73 +216,99 @@ def main():
             # Forward pass real batch through D
             output = discriminator(real_imgs).view(-1)
             # Calculate loss on all-real batch
-            errD_real = criterion(output, label)
+            err_disc_real = criterion(output, label)
             # Calculate gradients for D in backward pass
-            errD_real.backward()
-            D_x = output.mean().item()
+            err_disc_real.backward()
+            # Compute mean value for the discriminator on real MNIST images
+            D_x += output.mean().item()
 
-            ## Train with generated images batch
-            # Generate batch of latent vectors
-            noise = torch.randn(b_size, 1, 7, 7, device=device)
-            # Generate gen_imgs image batch with G
-            gen_imgs = generator(noise)
+            # # Train with generated images batch
+            gen_input, exp_nums = create_generator_input(b_size, device)
+            # Generate image batch with G
+            gen_imgs = generator(gen_input)
             label.fill_(fake_label)
-            # Classify all gen_imgs batch with the discriminator
+            # Classify the generated images with the discriminator
             output = discriminator(gen_imgs.detach()).view(-1)
             # Calculate discriminator's loss on the all-gen_imgs batch
-            errD_gen = criterion(output, label)
+            err_disc_gen = criterion(output, label)
             # Calculate the gradients for this batch, accumulated (summed) with previous gradients
-            errD_gen.backward()
-            D_G_z1 = output.mean().item()
+            err_disc_gen.backward()
             # Compute error of D as sum over the generated and the real images batches
-            errD = errD_real + errD_gen
+            err_discriminator = err_disc_real + err_disc_gen
             # Update D
             opt_disc.step()
+            # Compute mean value for the discriminator on generated images
+            D_G_z1 += output.mean().item()
+            loss_disc += err_discriminator.item()
 
             ############################
             # (2) Update G network: maximize log(D(G(z)))
             ###########################
             generator.zero_grad()
-            label.fill_(real_label)  # gen_imgs labels are real for generator cost
-            # Since we just updated D, perform another forward pass of all-gen_imgs batch through D
-            output = discriminator(gen_imgs).view(-1)
+            classifier.zero_grad()
+            label.fill_(real_label)  # The labels are real for generator cost (we try to be as real as possible!)
+            # Since we just updated D, perform another forward pass of the generated image batch through D
+            output_disc = discriminator(gen_imgs).view(-1)
+            # Also, infer the number the represent (or: they are supposed to...)
+            output_class = classifier(gen_imgs)
             # Calculate generator's loss based on this output
-            errG = criterion(output, label)
+            err_generator = (1 - alpha) * criterion(output_disc, label) + \
+                            alpha * nn.functional.nll_loss(output_class, exp_nums)
             # Calculate gradients for G
-            errG.backward()
-            D_G_z2 = output.mean().item()
+            err_generator.backward()
             # Update G
             opt_gen.step()
+            # Compute probabilities resp. accuracy for to display
+            D_G_z2 += output_disc.mean().item()
+            loss_gen += err_generator.item()
+            gen_acc += exp_nums.eq(output_class.argmax(dim=1)).sum().item() / b_size
 
             # Output training stats
-            if i % 50 == 0:
-                print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                      % (epoch, args.epochs, i, len(dataloader),
-                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+            freq = 50
+            if (i+1) % freq == 0:
+                print(f"[{epoch+1}/{args.epochs}][{i+1}/{len(dataloader)}]" +
+                      f"\tLoss_D: {loss_disc/freq:.4f}\tLoss_G: {loss_gen/freq:.4f}" +
+                      f"\tD(x): {D_x/freq:.4f}\tD(G(z)): {D_G_z1/freq:.4f} / {D_G_z2/freq:.4f}" +
+                      f"\tC(G(z): {gen_acc/freq:.4%}")
+                loss_disc, loss_gen, D_x, D_G_z1, D_G_z2, gen_acc = 0, 0, 0, 0, 0, 0
 
             # Save Losses for plotting later
-            losses_gen.append(errG.item())
-            losses_disc.append(errD.item())
-
-            # Check how the generator is doing by saving G's output on fixed_noise
-            if (iters % 500 == 0) or ((epoch == args.epochs-1) and (i == len(dataloader)-1)):
-                with torch.no_grad():
-                    gen_imgs = generator(fixed_noise).detach().cpu()
-                img_list.append(vutils.make_grid(gen_imgs, padding=2, normalize=True))
+            losses_gen.append(err_generator.item())
+            losses_disc.append(err_discriminator.item())
 
             iters += 1
 
-        plt.figure()
-        for i in range(min(24, gen_imgs.size(0))):
-            plt.subplot(4, 6, i+1)
-            plt.imshow(gen_imgs[i, 0, :, :].cpu().detach().numpy(), cmap='gray')
+        # End of Epoch, run once the fixed noise
+        gen_imgs = generator(fixed_gen_in)
+        output_class = classifier(gen_imgs)
+        derived_num = output_class.argmax(dim=1)
+        correct = fixed_exp_nums.eq(derived_num).sum().item()
+        accuracy = correct / fixed_gen_in.size(0)
+        # Move them back to the CPU
+        gen_imgs = gen_imgs.detach().cpu()
+        derived_num = derived_num.detach().cpu()
+        img_list.append(vutils.make_grid(gen_imgs, padding=2, normalize=True))
+
+        print(f"\nFixed Input: {epoch+1}/{args.epochs} epochs, " +
+              f"number classifier accuracy: {accuracy:.4%} ({correct} / {fixed_gen_in.size(0)})\n")
 
         plt.figure()
-        plt.title("Loss Development")
-        plt.plot(losses_gen, label='Generator loss')
-        plt.plot(losses_disc, label='Discriminator loss')
-        plt.legend()
-        plt.show()
+        for sample_no in range(min(60, gen_imgs.size(0))):
+            plt.subplot(6, 10, sample_no+1)
+            plt.title(f"{int(fixed_exp_nums[sample_no].item())} ({derived_num[sample_no]})")
+            plt.imshow(gen_imgs[sample_no, 0, :, :].cpu().detach().numpy(), cmap='gray')
+
+        plt.savefig(f"fixed_input_samples_epoch={epoch+1}_acc={int(100*gen_acc):02d}.png")
+
+        if epoch % 2 == 0:
+            plt.figure()
+            plt.title("Loss Development")
+            plt.plot(losses_gen, label='Generator loss')
+            plt.plot(losses_disc, label='Discriminator loss')
+            plt.legend()
+            plt.show()
+        else:
+            plt.close()
 
 
 if __name__ == '__main__':
